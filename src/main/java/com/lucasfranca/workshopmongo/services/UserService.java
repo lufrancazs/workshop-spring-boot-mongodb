989038a19fe7eba4e515ee8a2c6c09ba7e3bec21
Lucas Martins <lucasfranca_zs@hotmail.com>
2024-01-29 20:08:50 -0300

    User insert

--- src/main/java/com/lucasfranca/workshopmongo/services/UserService.java
+++ src/main/java/com/lucasfranca/workshopmongo/services/UserService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.lucasfranca.workshopmongo.dto.UserDTO;
 import com.lucasfranca.workshopmongo.entities.User;
 import com.lucasfranca.workshopmongo.repositories.UserRepository;
 import com.lucasfranca.workshopmongo.services.exception.ObjectNotFoundException;
@@ -23,5 +24,13 @@ public class UserService {
 	public User findyById(String id) {
 		Optional<User> user = repository.findById(id);		
 		return user.orElseThrow( () -> new ObjectNotFoundException(id));
+	}
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO userDTO) {
+		return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
 	}
 }
