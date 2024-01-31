15f792547d9b34405821770707c79198f83194b2
Lucas Martins <lucasfranca_zs@hotmail.com>
2024-01-31 15:24:38 -0300

    Getting post by Id

---
+++ src/main/java/com/lucasfranca/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.lucasfranca.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lucasfranca.workshopmongo.entities.Post;
+import com.lucasfranca.workshopmongo.repositories.PostRepository;
+import com.lucasfranca.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repository;
+
+	public Post findyById(String id) {
+		Optional<Post> post = repository.findById(id);
+		return post.orElseThrow(() -> new ObjectNotFoundException(id));
+
+	}
+}
