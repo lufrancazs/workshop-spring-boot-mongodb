0856f01b1558b3fd529ffb7059d9d2d416bdfc81
Lucas Martins <lucasfranca_zs@hotmail.com>
2024-02-02 17:27:24 -0300

    Simple query with @Query

--- src/main/java/com/lucasfranca/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/lucasfranca/workshopmongo/repositories/PostRepository.java
@@ -3,12 +3,16 @@ package com.lucasfranca.workshopmongo.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.lucasfranca.workshopmongo.entities.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String title);
 	
 	List<Post> findByTitleIgnoreCase(String title);
 
--- src/main/java/com/lucasfranca/workshopmongo/services/PostService.java
+++ src/main/java/com/lucasfranca/workshopmongo/services/PostService.java
@@ -22,6 +22,6 @@ public class PostService {
 	}
 	
 	public List<Post> findByTitle(String text) {
-		return repository.findByTitleIgnoreCase(text);
+		return repository.searchTitle(text);
 	}
 }
